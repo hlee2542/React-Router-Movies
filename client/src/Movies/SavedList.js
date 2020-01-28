591619cbb38f9caa1d5a3956c8a1ed5abecb4553
hlee2542 <ou40a9rtbp@gmail.com>
2020-01-28 13:51:52 -0800

    added navlinks to savedlist

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
+import {useHistory, NavLink} from 'react-router-dom';
 
 const SavedList = props => {
   let history = useHistory();
@@ -7,7 +7,9 @@ const SavedList = props => {
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <NavLink to={`/movies/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </NavLink>
       ))}
       <div className="home-button" onClick={() => history.push('/')}>Home</div>
     </div>
